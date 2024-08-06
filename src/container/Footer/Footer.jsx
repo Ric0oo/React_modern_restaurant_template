b5f3cb542d2ac42e672d2bea9c4b7c8ab77fd714
Ric0oo <58725640+Ric0oo@users.noreply.github.com>
2024-08-06 13:21:47 +0200

    Newsletter and FooterOverlay added to Footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { FiFacebook, FiInstagram, FiTwitter } from "react-icons/fi";
+import { FooterOverlay, Newsletter } from "../../components";
+import "./Footer.css";
 
-import './Footer.css';
-
-const Footer = () => (
-  <div>
-    Footer
-  </div>
-);
+const Footer = () => {
+  return (
+    <div className="app__footer section__padding">
+      <FooterOverlay />
+      <Newsletter />
+    </div>
+  );
+};
 
 export default Footer;
